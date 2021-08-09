7a340c101db78d3ee2d3e6da9fb10a1838a83268
pumpuidev <ernest.prorok@gmail.com>
2021-08-09 15:24:17 +0200

    feat: ujra az egesz

--- backend/src/models/service.model.js
+++ backend/src/models/service.model.js
@@ -1,1 +1,31 @@
 // 1. Feladat: pótold a service modellt.
+
+const mongoose = require('mongoose');
+
+const ServiceSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    since: {
+        type: Number,
+        required: true
+    },
+    street: {
+        type: String,
+    },
+    city: {
+        type: String,
+    },
+    country: {
+        type: String,
+    },
+    active: {
+        type: Boolean,
+        default: true
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Service', ServiceSchema);
